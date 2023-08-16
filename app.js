674f3cfd72b4b8322a8355e7bf399bbe4f621039
Artur Licks <alicks@atlassian.com>
2023-08-16 17:28:34 -0300

    New ejs tag

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config()
+require('dotenv').config();
 
 var standingsRouter = require('./routes/standings');
 var usersRouter = require('./routes/users');
